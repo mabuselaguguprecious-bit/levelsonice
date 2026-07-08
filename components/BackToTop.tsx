c1686eaaf2f77b8fdf05d2391eca026e22f3d0d1
mabuselaguguprecious-bit <mabuselaguguprecious@gmail.com>
2026-07-08 02:50:56 +0200

    Fully responsive update - mobile, tablet & desktop ready

--- components/BackToTop.tsx
+++ components/BackToTop.tsx
@@ -25,7 +25,7 @@ export default function BackToTop() {
   return (
     <button
       onClick={scrollToTop}
-      className={`fixed bottom-8 sm:bottom-6 right-6 sm:right-3 z-30 flex items-center gap-3 bg-white text-black px-35 sm:px-25 py-3 sm:py-4 rounded-2xl shadow-xl hover:bg-lime-400 transition-all duration-300 ${
+      className={`fixed bottom-8 sm:bottom-6 right-6 sm:right-3 z-1 flex items-center gap-5 bg-white text-black px-35 sm:px-5 py-3 sm:py-4 rounded-2xl shadow-xl hover:bg-lime-400 transition-all duration-300 ${
         showButton
           ? "opacity-100 translate-y-0"
           : "opacity-0 translate-y-8 pointer-events-none"
